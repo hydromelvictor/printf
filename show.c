d442bf9550c942aba5669c71d5da4a57ac7865d1
harkinder-dark <victorvaddely@gmail.com>
2022-08-06 15:27:33 +0000

    printf programming

--- show.c
+++ show.c
@@ -1,5 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 int _printf(const char *format, ...)
 {
-    return (0);
+va_list ap;
+int i;
+char *str = malloc(sizeof(char));
+if (format == NULL || str == NULL)
+{
+return (0);
+}
+va_start(ap, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%' && format[i - 1] != '\\')
+{
+switch (format[i + 1])
+{
+case 'c': write(1, va_arg(ap, char *), sizeof(char));
+i++;
+break;
+case 's': write(strlen(va_arg(ap, char *)), va_arg(ap, char *), sizeof(char));
+i++;
+break;
+case '%': write(1, &format[i + 1], sizeof(char));
+i++;
+break;
+}
+}
+else if (format[i] == '\\')
+{
+continue;
+}
+write(1, &format[i], sizeof(char));
+}
+return (0);
 }
--- test/main1.c
+++ test/main1.c
@@ -8,7 +8,6 @@ int main(void)
 {
     int len;
     int len2;
-
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     return (0);
