e6157de928dd3d6786299911a7e2910951f6a11e
harkinder-dark <victorvaddely@gmail.com>
2022-08-07 22:59:09 +0000

    printf programming

--- hexal.c
+++ hexal.c
@@ -5,41 +5,21 @@
 int hexal(unsigned int n, char c)
 {
     int i = 0, count = 0;
-    char *t = malloc(sizeof(char));
+    int *t = malloc(sizeof(int));
     if(t == NULL)
     {
     return 0;
     } 
     if(n < 16)
     {
-        if(c == 'x')
-        {
-            *t = (n==10) ? 'a' : (n==11) ? 'b' : (n==12) ? 'c' : (n==13) ? 'd' : (n==14) ? 'e' : 'f';
-        }
-        else {
-        *t = (n==10) ? 'A' : (n==11) ? 'B' : (n==12) ? 'C' : (n==13) ? 'D' : (n==14) ? 'E' : 'F';
-        }
+        *t = n;
         count++;
     }
     else
     {
         while (n >= 16)
         {
-            int p = n % 16;
-            if(p < 10)
-            {
-                t[i] = (char) p; 
-            }
-            else 
-            {
-                if(c == 'x')
-                {
-                    *t = (p==10) ? 'a' : (p==11) ? 'b' : (p==12) ? 'c' : (p==13) ? 'd' : (p==14) ? 'e' : 'f';
-                }
-                else {
-                    *t = (p==10) ? 'A' : (p==11) ? 'B' : (p==12) ? 'C' : (p==13) ? 'D' : (p==14) ? 'E' : 'F';
-                }
-            }
+            t[i]= n % 16;
             n /= 16;
             i++;
             count++;
@@ -48,8 +28,30 @@ int hexal(unsigned int n, char c)
     }
     for(; i >= 0; i--)
     {
-        char c = t[i] ;
-        write(1, &c, sizeof(char));
+        int p = t[i] + '0';
+        if(p < 10)
+        {
+            write(1, &p, sizeof(int));
+        }
+        else
+        {
+            char s = ' ';
+            switch (p) {
+                case 10 : s = (c == 'x') ? 'a' : 'A'; 
+                break;
+                case 11 :  s = (c=='x') ? 'b' : 'B';
+                break;
+                case 12 : s = (c=='x') ? 'c' : 'C';
+                break;
+                case 13 : s = (c=='x') ? 'd' : 'D';
+                break;
+                case 14 : s = (c=='x') ? 'e' : 'E';
+                break;
+                case 15 : s = (c=='x') ? 'f' : 'F';
+                break;
+            }
+        write(1, &s, sizeof(char));
+        }
     }
     return (count);
 }
