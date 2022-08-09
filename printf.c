255b2c12bbf2d7c7407fcd383f00f948072cf69c
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-08-09 08:11:23 +0000

    fixing some betty warnings

--- printf.c
+++ printf.c
@@ -12,15 +12,14 @@
 
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int count = 0;
-
-    if(format == NULL)
-    {
-        return (-1);
-    }
-    va_start(ap, format);
-    count = vfprintf(stdout, format, ap);
-    va_end(ap);
-    return (count);
+va_list ap;
+int count = 0;
+if (format == NULL)
+{
+return (-1);
+}
+va_start(ap, format);
+count = vfprintf(stdout, format, ap);
+va_end(ap);
+return (count);
 }
